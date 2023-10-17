98869649c3659103c7cb1845f6bf43de92296e0f
Danson Lino <danson.lino@student.moringaschool.com>
2023-10-17 17:16:10 +0300

    let's commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+// function saturdayFun with a default activity
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+   } 
+   console.log(saturdayFun());
+    console.log(saturday("bathe my dog"));
+  
+  
+  // Define a function expression mondayWork with a default activity of "go to the office"
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  console.log(mondayWork());
+  console.log(mondayWork("work from home"))
+  
+  // Define a function wrapAdjective that takes a string as a parameter and returns a function
+  function wrapAdjective() {
+    let result = wrapAdjective('*')
+      let emphatic = result("a hard worker")
+      return(`you are ${result} ${emphatic} ${result}!`);
+    }
+ 
